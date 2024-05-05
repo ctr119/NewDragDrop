9e3c9cc8bfc18f3a90987b89d3c04b144ee4ca13
Víctor Barrios <sygmaz19@gmail.com>
2024-05-05 22:22:12 +0200

    Initial Commit

---
+++ NewDragDrop/NewDragDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewDragDropApp.swift
+//  NewDragDrop
+//
+//  Created by Víctor Barrios Sánchez on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct NewDragDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
